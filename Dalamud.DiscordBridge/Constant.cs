e911f1c725866e144b25e1801f1ea5daeab674b9
goat <16760685+goaaats@users.noreply.github.com>
2020-12-06 11:36:28 +0100

    add help & kind list links

--- Dalamud.DiscordBridge/Constant.cs
+++ Dalamud.DiscordBridge/Constant.cs
@@ -8,8 +8,8 @@ namespace Dalamud.DiscordBridge
 {
     static class Constant
     {
-        public const string HelpLink = "";
-        public const string KindListLink = "";
+        public const string HelpLink = "https://github.com/goaaats/Dalamud.DiscordBridge/wiki/Setup-Guide";
+        public const string KindListLink = "https://github.com/goaaats/Dalamud.DiscordBridge/wiki/Chat-kinds";
         public const string DiscordJoinLink = "https://discord.gg/3NMcUV5";
         public const string LogoLink = "https://raw.githubusercontent.com/goatcorp/DalamudAssets/master/UIRes/logo.png";
     }
